23d3c8a8f8cc118d80f8d279998c7c63fe76f2b7
vaidehi2401 <dholevaidehi2@gmail.com>
2025-04-15 18:27:26 +0530

    Added user model

--- models/user.js
+++ models/user.js
@@ -1,16 +1,35 @@
-const Sequelize = require('sequelize');
-
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+class User{
+  constructor(id, name, email){
+    this._id = new mongodb.ObjectId(id);
+    this.name = name;
+    this.email = email;
+  }
+  save(){
+    const db = getDb();
+    return db.collection('users')
+    .insertOne(this)
+    .then((result)=>{
+      console.log(result);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+  static fetchUserById(userId){
+    const db = getDb();
+    return db.collection('users')
+    .find({_id: new mongodb.ObjectId(userId)})
+    .next()
+    .then(user=>{
+      console.log(user);
+      return user;
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
+}
 
 module.exports = User;
